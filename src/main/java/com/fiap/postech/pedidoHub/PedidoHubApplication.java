912d11d6879d137dd133be623007f72fa87643e5
Emerson Soares <emersonsoares269@icloud.com>
2025-06-25 20:58:47 -0300

    primeiro commit

---
+++ src/main/java/com/fiap/postech/pedidoHub/PedidoHubApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.postech.pedidoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoHubApplication.class, args);
+	}
+
+}
