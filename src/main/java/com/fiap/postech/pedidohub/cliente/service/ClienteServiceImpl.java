ea20d9ea7159a4fea2cedb67fe42134f42bba06b
Emerson Soares <emersonsoares269@icloud.com>
2025-07-04 20:18:41 -0300

    ajuste com try-catch

--- src/main/java/com/fiap/postech/pedidohub/cliente/service/ClienteServiceImpl.java
+++ src/main/java/com/fiap/postech/pedidohub/cliente/service/ClienteServiceImpl.java
@@ -7,6 +7,7 @@ import com.fiap.postech.pedidohub.cliente.domain.exceptions.*;
 import com.fiap.postech.pedidohub.cliente.domain.model.Cliente;
 import com.fiap.postech.pedidohub.cliente.gateway.port.ClienteRepositoryPort;
 import com.fiap.postech.pedidohub.cliente.gateway.port.ClienteServicePort;
+import com.fiap.postech.pedidohub.config.ErroInternoException;
 import com.fiap.postech.pedidohub.utils.ConstantUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,27 +24,49 @@ public class ClienteServiceImpl implements ClienteServicePort {
     @Override
     public ClienteDto cadastraCliente(ClienteRequest request) {
 
-        Cliente cliente = ClienteMapper.INSTANCE.requestToDomain(request);
+        try {
+            Cliente cliente = ClienteMapper.INSTANCE.requestToDomain(request);
 
+            validarCliente(cliente);
+
+            return repositoryPort.cadastraCliente(cliente);
+
+        } catch (InvalidCpfException | ClienteExistsException | InvalidDataNascimentoException |
+                 InvalidCepException | InvalidEstadoException e) {
+            throw e;
+
+        } catch (Exception e) {
+            log.error("Erro inesperado ao cadastrar cliente", e);
+            throw new ErroInternoException("Erro interno ao tentar cadastrar cliente: " + e.getMessage());
+        }
+    }
+
+    private void validarCliente(Cliente cliente) {
         if (!cliente.cpfFormatoValido()) {
             log.warn("CPF com formato inválido: {}", cliente.getCpfCliente());
             throw new InvalidCpfException(ConstantUtils.CPF_INVALIDO);
-        } else if (repositoryPort.findByCpfCliente(cliente.getCpfCliente()).isPresent()) {
+        }
+
+        if (repositoryPort.findByCpfCliente(cliente.getCpfCliente()).isPresent()) {
             log.warn("Cliente já cadastrado com o CPF: {}", cliente.getCpfCliente());
             throw new ClienteExistsException(ConstantUtils.CLIENTE_JA_EXISTE);
-        } else if (!cliente.nascimentoNaoEhFuturo()) {
+        }
+
+        if (!cliente.nascimentoNaoEhFuturo()) {
             log.warn("Data de nascimento não pode ser no futuro: {}", cliente.getDataNascimento());
             throw new InvalidDataNascimentoException(ConstantUtils.DATA_NASCIMENTO_INVALIDA);
-        } else if (!cliente.cepFormatoValido()) {
+        }
+
+        if (!cliente.cepFormatoValido()) {
             log.warn("CEP com formato inválido: {}", cliente.getCep());
             throw new InvalidCepException(ConstantUtils.CEP_INVALIDO);
-        } else if (!cliente.estadoFormatoValido()) {
+        }
+
+        if (!cliente.estadoFormatoValido()) {
             log.warn("Estado com formato inválido: {}", cliente.getEstado());
             throw new InvalidEstadoException(ConstantUtils.ESTADO_INVALIDO);
         }
-
-        // SE ESTIVER TUDO CERTO, CHAMA A IMPLEMENTAÇÃO DO REPOSITORY PARA CADASTRAR O CLIENTE
-        return repositoryPort.cadastraCliente(cliente);
     }
+
 
 }
